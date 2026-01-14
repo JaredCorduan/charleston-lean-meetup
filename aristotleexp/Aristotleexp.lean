fbddd27cf8c1fd81a5c9bad75ed73a10b9b4fc4c
Benoit Razet <benoit.razet@galois.com>
2026-01-13 19:06:14 -0500

    First try Aristotle

---
+++ aristotleexp/Aristotleexp.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `Aristotleexp` library.
+-- Import modules here that should be built as part of the library.
+import Aristotleexp.Basic
+import Aristotleexp.semiring1
